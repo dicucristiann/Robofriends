e541aabc9b357c7b4c01388f37e4ee9b008d6a08
dicucristiann <dicucristian24@gmail.com>
2022-09-17 21:21:31 +0300

    fisrt part of robo-friends

---
+++ src/robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Luyss Gram',
+        username: 'Luy',
+        email: 'Lgram@september.biz'
+
+    },
+    {
+        id: 2,
+        name: 'Lore Racz',
+        username: 'Lor3',
+        email: 'raczL@september.tv'
+
+    },
+    {
+        id: 3,
+        name: 'Cristi Marginean',
+        username: 'Le-Cris',
+        email: 'L-cris@mail.com'
+
+    },
+    {
+        id: 4,
+        name: 'Racz Darius',
+        username: 'R&D',
+        email: 'racz@mailato.rd'
+
+    },
+    {
+        id: 5,
+        name: 'Larisa Marina',
+        username: 'Ary',
+        email: 'lma@email.com'
+
+    },
+    {
+        id: 6,
+        name: 'Delia Marginean',
+        username: 'D&M',
+        email: 'D&M@solution.biz'
+
+    },
+    {
+        id: 7,
+        name: 'Leanne Howell',
+        username: 'Antonim',
+        email: 'sincere@april.biz'
+
+    },
+    {
+        id: 8,
+        name: 'Bauch Lenny',
+        username: 'bau-01',
+        email: 'bauch@gentleman.com'
+
+    },
+    {
+        id: 9,
+        name: 'Eduard Grey',
+        username: 'Edy',
+        email: 'eduard@crpotu.com'
+
+    },
+    {
+        id: 10,
+        name: 'Jane Doe',
+        username: 'jan-e',
+        email: 'JaneDoe@mail.biz'
+
+    },
+]
